52a858ecff3258d659aca064bf94747c6cea1121
SuhailHasan <64515577+SuhailHasan@users.noreply.github.com>
2020-04-29 10:52:59 +0530

    Update MaiLBomB.py

--- MaiLBomB.py
+++ MaiLBomB.py
@@ -5,7 +5,7 @@ import sys
 import time
 from colorama import init, Style, Back, Fore
 
-print """
+print ("""
     __  ___      _ __    ____                  ____ 
    /  |/  /___ _(_) /   / __ )____  ____ ___  / __ )
   / /|_/ / __ `/ / /   / __  / __ \/ __ `__ \/ __  |
@@ -15,22 +15,22 @@ print """
                                                     (Made By Suhail)
                                                     
                             
-"""
+""")
 def BomEmail():
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
 
-server = raw_input ('MailServer 1.Gmail/2.Yahoo: ')
-user = raw_input('Email: ')
-passwd = raw_input('Password: ')
+server = input ('MailServer 1.Gmail/2.Yahoo: ')
+user = input('Email: ')
+passwd = input('Password: ')
 
 
-to = raw_input('\nTo: ')
+to = input('\nTo: ')
 #subject = raw_input('Subject: ')
-body = raw_input('Message: ')
-total = input('Number of send: ')
+body = input('Message: ')
+total = int(input('Number of send: '))
 
 if server == 'gmail' or '1' or 'Gmail':
     smtp_server = 'smtp.gmail.com'
@@ -42,7 +42,7 @@ else:
     print 'Kindly Enter Your Answer in 1 or 2 in Mail Server.'
     sys.exit()
 
-print ''
+print ('')
 
 try:
     server = smtplib.SMTP(smtp_server,port)
@@ -52,12 +52,12 @@ try:
     server.login(user,passwd)
     for i in range(1, total+1):
         subject = os.urandom(9)
-        msg = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
+        msg = 'From: ' + user + '\nSubject: ' + str(subject) + '\n' + body
         server.sendmail(user,to,msg)
-        print "\r[+]E-mails sent: %i" % i
+        print ("\r[+]E-mails sent: %i" % i)
         sys.stdout.flush()
     server.quit()
-    print """
+    print ("""
         __  ___      _ __    ____                  ____ 
        /  |/  /___ _(_) /   / __ )____  ____ ___  / __ )
       / /|_/ / __ `/ / /   / __  / __ \/ __ `__ \/ __  |
@@ -67,10 +67,10 @@ try:
                     +-+-+-+-+-+-+-+
                     |E|n|j|o|y|:|)|
                     +-+-+-+-+-+-+-+                 
-    """
+    """)
 except KeyboardInterrupt:
-    print '[-] Canceled'
+    print ('[-] Process Canceled By User')
     sys.exit()
 except smtplib.SMTPAuthenticationError:
-    print '\n[!] Allow access to less secure apps on your gmail account. https://www.google.com/settings/security/lesssecureapps'
+    print ('\n[!] Allow access to less secure apps on your gmail account. https://www.google.com/settings/security/lesssecureapps')
     sys.exit()
